61d14e57a27d7e8c0141fd922ed5b13431a99a1b
Andoy04 <carpio.andoy@gmail.com>
2022-06-24 01:19:50 +0800

    Added simple user model and view

--- user_registration_api/user_registration/models.py
+++ user_registration_api/user_registration/models.py
@@ -1,3 +1,9 @@
+import email
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email_address = models.CharField(max_length=255, primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    active = models.BooleanField()
--- user_registration_api/user_registration/views.py
+++ user_registration_api/user_registration/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import User
+ 
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    # define queryset
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
